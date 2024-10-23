0d83baef257f04eb78d33e7b62a898c9280e3758
hiroshi.imaizumi <hiroshi.imaizumi@bigtreetc.com>
2024-10-23 16:38:45 +0900

    add 3 pages

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,5 @@
 import Announcements from "@/components/Announcements";
-import EventCalendar from "@/components/EventCalendar";
+import BigCalendar from "@/components/BigCalender";
 
 const StudentPage = () => {
   return (
@@ -7,12 +7,12 @@ const StudentPage = () => {
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
         <Announcements />
       </div>
     </div>
