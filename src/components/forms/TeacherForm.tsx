d93cc8079ac382191e2bd0b5d48d038f65c3b701
hiroshi.imaizumi <hiroshi.imaizumi@bigtreetc.com>
2024-11-17 19:02:35 +0900

    install and schema

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters lomg!" })
+    .max(20, { message: "Username must be at most 20 characters lomg!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" }),
+  firstname: z.string().min(1, { message: "First name is required!" }),
+  lastname: z.string().min(1, { message: "Last name is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+  address: z.string().min(1, { message: "Address is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required!" }),
+  img: z.instanceof(File, { message: "Image is required!" }),
+});
+
+const TeacherForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  return (
+    <div className="">
+      <input type="text" />
+    </div>
+  );
+};
+
+export default TeacherForm;
