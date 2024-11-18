db2c5d36a39a2b71da79bfabd05ec218f1d4a4e8
hiroshi.imaizumi <hiroshi.imaizumi@bigtreetc.com>
2024-11-18 18:12:22 +0900

    username input

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -35,10 +35,36 @@ const TeacherForm = ({
     formState: { errors },
   } = useForm({ resolver: zodResolver(schema) });
 
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <div className="">
-      <input type="text" />
-    </div>
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">Create a new teacher</h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentication Information
+      </span>
+      <div className="flex flex-col gap-2 w-full md:w-1/4">
+        <label className="text-xs text-gray-500">Username</label>
+        <input
+          type="text"
+          {...register("username")}
+          className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+        />
+        {errors.username?.message && (
+          <p className="text-xs text-red-500">
+            {errors.username?.message.toString()}
+          </p>
+        )}
+      </div>
+      <span className="text-xs text-gray-400 font-medium">
+        Personal Information
+      </span>
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
   );
 };
 
